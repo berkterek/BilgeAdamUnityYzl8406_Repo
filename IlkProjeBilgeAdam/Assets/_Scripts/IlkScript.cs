d825dea4d745b11de4c20df9589ae3158f330388
berkterek <42406263+berkterek@users.noreply.github.com>
2023-05-21 09:23:39 +0300

    feat: downloads and resimler added

---
+++ IlkProjeBilgeAdam/Assets/_Scripts/IlkScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IlkScript : MonoBehaviour
+{
+    //Bu bir yorum satiridir
+    //Bu bir method'tur iconu mor kuptur
+    /*
+     *
+     * coklu
+     * yorum
+     * satiri
+     *
+     */
+    void Start()
+    {
+        //Debug.Log unity uzerinde console uzerine yazdirmaya yarar.
+        Debug.Log("Hello World!");
+    }
+}
