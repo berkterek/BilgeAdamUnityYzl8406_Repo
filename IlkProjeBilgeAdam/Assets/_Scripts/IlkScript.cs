28217830144e6e8fc76e46de3bfaf849abc92f22
berkterek <42406263+berkterek@users.noreply.github.com>
2023-05-21 14:31:13 +0300

    feat: odev script created

--- IlkProjeBilgeAdam/Assets/_Scripts/IlkScript.cs
+++ IlkProjeBilgeAdam/Assets/_Scripts/IlkScript.cs
@@ -4,14 +4,20 @@ public class IlkScript : MonoBehaviour
 {
     //Global Degisken veya field
     public int CurrentHealth = 200;
-    public int SonicCoin = 1000; //++ => 1001 1002 1003 / direkt 0
+    public int SonicCoin = 1000; //++ => 1001 1002 1003 / direkt 0 //default 100
     public int OrnekTamSayi; //tam sayi
     public float OrnekOndalikliSayi; //ondalikli sayimiz
     public GameObject OrnekGameObject; //GameObject kendisi
     public Vector2 Vector2; //2d 
     public Vector3 Vector3; //3d
     public string OrnekString;
-    
+    public GameObject BosGecilebilir1;
+    public string BosGecilebilir2;
+    public int BosGecilemeyen1;
+    public float BosGecilemeyen2;
+    public Vector2 BosGecilemeyen3;
+    public Vector3 BosGecilemeyen4;
+
     //field'lara bir deger atayabilirz ama islem yapamayiz field'lara islemleri method veya property icinde yapariz
     //SonicCoin = 100 + 200;
 
@@ -31,7 +37,11 @@ public class IlkScript : MonoBehaviour
         Debug.Log("Hello World!");
 
         //Local Degisken veya local variable
-        string value = "Hello World Again!";
+        //value icine Hello World Again! string degerini atadim.
+        string value = "Hello World Again!"; 
+        Debug.Log(value); //value icindeki degeri Debug.Log icinde yazdirdim 
+        value = value + " " + "Hello Again and Again"; //value degerini maniple ettim ve birseyler ekledim => "Hello World Again! Hello Again and Again"
+        Debug.Log(value); //ayni value degerini debug.Log icinde yazdirdim
         char value1 = 'H';
         char value2 = 'e';
 
@@ -85,6 +95,7 @@ public class IlkScript : MonoBehaviour
 
     public int Coin = 100;
     public int SellValue = 10;
+    public int BuyValue = 10;
 
     [ContextMenu(nameof(BuyItem))]
     public void BuyItem()
@@ -94,17 +105,46 @@ public class IlkScript : MonoBehaviour
          * 1.Coin degeri ve satin alinan fiyat birimin birbirinden cikarilmasi ve islem sonunu
          * 2.Yapilan islemin oyuncuya veya kullanici kisisine gosterilmesi
          */
-        int result = Coin - SellValue;
-        Debug.Log(result);
-        Coin = result;
-        Debug.Log(Coin);
+
+        int result = Coin - BuyValue;
+
+        //eger result degeri 0'dan buyuk veya esit ise satin alma islemini yap
+        //eger result degeri 0'dan kucukse
+        //if eger anlamindadir c#(csharp) icinde sart anlami katar ornegin sonuc 0'dan buyukse veya sonuc 0'a esit ise gibi sartlari kontrol ettiririz
+        //result > 0 => result'in degeri 0'dan buyukse
+        //result < 0 => result'in degeri 0'dan kucukse
+        //result >= 0 => result'in degeri 0'dan buyukse veya 0'a esitse
+        //result <= 0 => result'in degeri 0'dan kucukse veya 0'a esitse
+        //result == 0 => result'in degeri 0'a esit mi? tek esittir "=" atama operatorudur // cift esittir "==" bir sart operatorudur yani esit mi diye soran operatordur
+        if (result >= 0)
+        {
+            //Debug.Log(result);
+            Coin = result;
+            //int => 10
+            //string = "10"
+            string stringResult = Coin.ToString(); //tipten tipi direkt atamlar yapamayiz burda oldugu gibi int tipli bir deger string'e veya string bir degiskene atmak istersek onu convert etmeliyiz
+            //ToString() method'u int olan degeri string'e convert etmis oldu ve bunun bu string'e donen int degeri artik matematiksel hic bir degeri yoktur
+            string message = "Satis islemi gerceklesti " + stringResult;
+            Debug.Log(message);
+        }
+        else // else tap tap yazarsaniz kendi otomatik yazicaktir scope'lariyla beraber
+        {
+            //if else if fark etmez else yapisi yukaridaki sartlardan biri calismaz ise else sarti herzmaan calir
+            //result 0'dan buyuk veya esit degilse 0'dan kucuktur yani satir islemi para veya coin degeri yetmedigi icin gerceklestirilemez
+            //Debug.Log(Coin);
+            Debug.Log("Oyuncu parasi veya coin yeterli degildir");
+        }
     }
 
     //ContextMenu bir attribute'tur ve attribute'lar bizim methodlarimiza nitelik veya ozellik katmalaridir bu ContextMenu attribute bu methodlarin editor inspector uzerinde script'timize sag tikladigmiizda bu methodlari cagirmamizi saglar
     [ContextMenu(nameof(SellItem))]
     public void SellItem()
     {
-
+        //Coin = Coin + SellValue;
+        Coin += SellValue; // Coint = Coin + SellValue; / + - * /
+        //Coin++; Coin += 1 / Coin = Coin + 1
+        //Coin--; Coin -= 1 / Coin = Coin - 1
+        Debug.Log(Coin);
     }
 
     [ContextMenu(nameof(SonicTakeDamage))]
@@ -119,5 +159,40 @@ public class IlkScript : MonoBehaviour
         int damage = 50;
         CurrentHealth = CurrentHealth - damage;
         Debug.Log(CurrentHealth);
+    }
+
+    [ContextMenu(nameof(IfElseOrnek))]
+    public void IfElseOrnek()
+    {
+        //Random.Range method bize rasgele sayi urutmemizi saglar
+        int randomNumber = Random.Range(0, 100);
+
+        //if - else
+        //if - else if ... - else
+        //if
+        //if - else if ...
+        if (randomNumber < 50)
+        {
+            //birinci string combine yontemi 
+            //randomNumber veya randomNumber.ToString() yazilablir eger ToString yazmazsaniz bile c# bunu kendi otomatik yapicaktir
+            Debug.Log("Random Sayi 50'den kucuktur " + randomNumber.ToString());
+        }
+        else if (randomNumber > 50)
+        {
+            //$ dolar isareti ile string combine edeiblirz iki yontemde ayni mantikta calisir hic bir farki yoktur
+            Debug.Log($"Random Sayi 50'den buyuktur {randomNumber}");
+        }
+        else if (false)
+        {
+
+        }
+        else if (false)
+        {
+
+        }
+        else
+        {
+            Debug.Log("Random Sayi 50'e esittir");
+        }
     }
 }
---
+++ IlkProjeBilgeAdam/Assets/_Scripts/OdevScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OdevScript : MonoBehaviour
+{
+    //1.1 Sayi1 Sayi2 veya Number1 Number2 adinda iki tane field olsun bu field'lari runtime uzerinde inspector'dan degistirebilelim
+    //1.2 4 tane method yazalim toplama cikarma carpma ve bolme islemi yapan burda sonuc her method calistiginda sayi1 ve sayi2 degeinin isleminin sonucunu Debug.Log() icinde ornegin toplama icin "Sayi1={Sayi1'in degeri} + Sayi2={Sayi2'in degeri} = {Sonucun degeri}" bu sekil yazdirmasini yapailm
+    
+    //2.1 Random.Range bir sayi olusturalim 0 ile 500 arasinda olsun
+    //2.2 bu sayinin 250'den kucuk veya buyuk olup olmadina bakalim ve bunu console(debug.log) icinde yazdiralim ornek "Random Sayi 250'dne kucuktur => {RandomSayiDegeri}" gibi
+    //2.3(bonus) 250'den kucuk ama 150'den buyuk ise console uzerinde yazdiralim 250'den buyuk ama 450'den kucuk seklinde yazdiralim
+}
