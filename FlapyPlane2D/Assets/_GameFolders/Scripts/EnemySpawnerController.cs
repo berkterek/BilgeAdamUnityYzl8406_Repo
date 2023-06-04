3211198045f0f0d36cb3e7072ec1c57848ece395
berkterek <42406263+berkterek@users.noreply.github.com>
2023-06-04 11:40:31 +0300

    refactor: min and max spawn time added

--- FlapyPlane2D/Assets/_GameFolders/Scripts/EnemySpawnerController.cs
+++ FlapyPlane2D/Assets/_GameFolders/Scripts/EnemySpawnerController.cs
@@ -1,27 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawnerController : MonoBehaviour
 {
     public EnemyController Prefab;
+    //Range attribute bize inspector uzerinde degerlerle oynammaizi saglan bir slider cikarir
+    [Range(3f,10f)]
     public float _maxSpawnTime = 5f;
+    [Range(0.1f,3f)]
+    public float _minSpawnTime = 1f;
 
     float _currentTime = 0f;
+    float _randomNumber = 0f;
+
+    void Start()
+    {
+        SetRandomTimeAndSetCurrentTimeToZero();
+    }
 
     void Update()
     {
         _currentTime += Time.deltaTime;
 
-        if (_currentTime > _maxSpawnTime)
+        if (_currentTime > _randomNumber)
         {
             //Instantiate method bizden bir prefab ister ve bu prefab'in bu method clone'unu olusturur
             //clone olusturmak 0'dan olusturmaktan daha performanslidir bu yuzden prefab uzerinden clone yaratiriz
             //bizim kullandigmiz method ikinci paramtre olarak pozisyon ister
             //bizim kullandiigmiz yontem ucuncu parametre olarak rotation ister burdaki quaternion.identity ise bize Vector3.zero ile ayni isi yapar yani 0,0,0,0 sonucunu verir
             Instantiate(Prefab, transform.position, Quaternion.identity);
-            _currentTime = 0f;
+            SetRandomTimeAndSetCurrentTimeToZero();
         }
+    }
+
+    private void SetRandomTimeAndSetCurrentTimeToZero()
+    {
+        _currentTime = 0f;
+        _randomNumber = Random.Range(_minSpawnTime, _maxSpawnTime);
     }
 }
