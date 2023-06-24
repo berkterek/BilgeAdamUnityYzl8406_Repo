d4e60c1b814c55d0515ce2392afc2639f6c40794
berkterek <42406263+berkterek@users.noreply.github.com>
2023-06-24 14:11:40 +0300

    feat: homework

--- SpaceShipWars2D/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ SpaceShipWars2D/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -6,6 +6,16 @@ namespace SpaceShipWars2D.Controllers
 {
     public class PlayerController : MonoBehaviour
     {
+        /*
+         *  ---------------------Odev---------------------
+         *  1.Input burda alip _mover ile kullanmak sag sol asagi yukari islemleri
+         *  2.InputReader icinde cevabi var ama once o tarafi kendimiz yazmayi deniyelim google arastirmasi hatta chatgpt bile olur isin icinden cikamazsaniz cevaba bakaiblirisniz
+         * 
+         *  --------------------Bonus---------------------
+         *  3.MoveWithTranslate ile yurutme yaptik baska bir yurutme yontemi kullanalim bu yontem size kalmistir secmek ister rigidbody2d ister transform ama ikinci bir yurutme yontemi yazailm ve MoveWithTranslate yerine burda kullanalim ayni _mover ile yani IMover tipini degistirmeden kullanalim
+         * 
+         */
+        
         IMover _mover;
 
         void Awake()
---
+++ SpaceShipWars2D/Assets/_GameFolders/Scripts/Concretes/Inputs/InputReader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SpaceShipWars2D.Inputs
+{
+    public class InputReader
+    {
+        
+    }   
+    
+    /*
+     * cevap
+     *
+     * // public Vector2 Direction { get; private set; }
+        //
+        // public InputReader()
+        // {
+        //     GameInputActions input = new GameInputActions();
+        //
+        //     input.Player.Move.performed += HandleOnMovement;
+        //     input.Player.Move.canceled += HandleOnMovement;
+        //     
+        //     input.Enable();
+        // }
+        //
+        // void HandleOnMovement(InputAction.CallbackContext context)
+        // {
+        //     Direction = context.ReadValue<Vector2>();
+        // }
+     * 
+     */
+}
