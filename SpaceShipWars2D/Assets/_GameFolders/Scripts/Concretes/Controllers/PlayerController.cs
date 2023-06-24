c7db37cff35dbd44bc4089e9c3e62281d2ef946e
berkterek <42406263+berkterek@users.noreply.github.com>
2023-06-24 13:44:36 +0300

    feat: player prefab, Event System replace new input system and GameInputActions created

---
+++ SpaceShipWars2D/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShipWars2D.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        
+    }    
+}
