c7d057dd72444cf67dfdd40de4f383b27511405f
berkterek <42406263+berkterek@users.noreply.github.com>
2023-06-24 11:31:06 +0300

    refactor: damage fx added to enemies

--- CircleBall3D/Assets/_GameFolders/Scripts/Combats/AttackWithAnimation.cs
+++ CircleBall3D/Assets/_GameFolders/Scripts/Combats/AttackWithAnimation.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using CircleBall3D.Controllers;
 using UnityEngine;
 
@@ -6,12 +7,23 @@ namespace CircleBall3D.Combats
     public class AttackWithAnimation : MonoBehaviour
     {
         [SerializeField] EnemyController _enemyController;
+        [SerializeField] GameObject _particleFx;
 
         //Attack method'u animasyon event'i ile calisir
         //Animation event ise Attack animsyonuna cift tikladiginizda cizgi halinde gorursunuz 
         public void Attack()
         {
+            StartCoroutine(PlayFxAsync());
             _enemyController.PlayerController.Health.TakeHit(_enemyController.Stats.Damage);
+        }
+
+        private IEnumerator PlayFxAsync()
+        {
+            _particleFx.gameObject.SetActive(true);
+
+            yield return new WaitForSeconds(0.5f);
+            
+            _particleFx.gameObject.SetActive(false);
         }
     }    
 }
--- CircleBall3D/Assets/_GameFolders/Scripts/Managers/GameManager.cs
+++ CircleBall3D/Assets/_GameFolders/Scripts/Managers/GameManager.cs
@@ -1,4 +1,3 @@
-using System;
 using CircleBall3D.Enums;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -22,7 +21,7 @@ namespace CircleBall3D.Managers
 
         void Start()
         {
-            _currentLevel = 0;
+            _currentLevel = SceneManager.GetActiveScene().buildIndex;
         }
 
         //singleton pattern bir design patterndir ve diger design pattern'lerle ayni mantiktadir yani kaliplasmias sorunlarin kaliplasmis cozumudur burda bir singleton yontemi kullaniyoruz mantigi sudur static bir instance icinde ilk gelen referansi tutuyoruz ve eger Instance null ise ilk referansi sakla ve oyun sonuna kadar onu kullan demis olduk unity game engine kullandigimz icin burda hersey game object uzerinden yurur ve burda bir singleton mantigi kullandimizdan ayni seyi o referansi saklayan GameObject icinde yapariz DontDestroyOnLoad method'u o game object'in butun oyun boyunca saklanacaigni ifade eder else icine gelicek olursa yeni olucan GameObject'leri yok et dmeis olduk boylelikle hem class referansi olarak hemde game object olarak bir tekillik yakalamis olduk.
