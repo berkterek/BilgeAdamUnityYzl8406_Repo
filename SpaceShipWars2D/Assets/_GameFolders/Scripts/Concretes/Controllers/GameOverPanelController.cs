085206c836cb1194893e3269980dbd3ef021ae21
berkterek <42406263+berkterek@users.noreply.github.com>
2023-07-09 12:10:25 +0300

    refactor: game over panel can fadeout

--- SpaceShipWars2D/Assets/_GameFolders/Scripts/Concretes/Controllers/GameOverPanelController.cs
+++ SpaceShipWars2D/Assets/_GameFolders/Scripts/Concretes/Controllers/GameOverPanelController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using SpaceShipWars2D.Managers;
 using UnityEngine;
 
@@ -5,6 +6,7 @@ namespace SpaceShipWars2D.Controllers
 {
     public class GameOverPanelController : MonoBehaviour
     {
+        [SerializeField] float _duration = 3f;
         [SerializeField] CanvasGroup _canvasGroup;
 
         void OnValidate()
@@ -26,9 +28,23 @@ namespace SpaceShipWars2D.Controllers
         
         void HandleOnGameOvered()
         {
-            _canvasGroup.alpha = 1f;
+            StartCoroutine(HandleOnGameOverAsync());
+        }
+
+        private IEnumerator HandleOnGameOverAsync()
+        {
+            yield return new WaitForSeconds(2f);
+            
             _canvasGroup.interactable = true;
             _canvasGroup.blocksRaycasts = true;
+            
+            float timer = 0f;
+            while (_canvasGroup.alpha < 1f)
+            {
+                timer += Time.deltaTime;
+                _canvasGroup.alpha = Mathf.Lerp(_canvasGroup.alpha, 1f, timer / _duration);
+                yield return null;
+            }
         }
     }    
 }
