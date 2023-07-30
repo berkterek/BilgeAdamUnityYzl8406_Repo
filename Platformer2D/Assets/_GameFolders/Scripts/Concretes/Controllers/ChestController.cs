6ff7f52edfa8b8601d0032429f1d8bf6220a5f95
berkterek <42406263+berkterek@users.noreply.github.com>
2023-07-30 13:29:59 +0300

    feat: chect prefab created and check can open with spend coin

--- Platformer2D/Assets/_GameFolders/Scripts/Concretes/Controllers/ChestController.cs
+++ Platformer2D/Assets/_GameFolders/Scripts/Concretes/Controllers/ChestController.cs
@@ -1,12 +1,16 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 namespace Platformer2D.Controllers
 {
     public class ChestController : MonoBehaviour
     {
+        [SerializeField] int _spendCoin = 5;
         [SerializeField] GameObject _itemHolder;
         [SerializeField] Animator _animator;
         [SerializeField] Collider2D _collider2D;
+        [SerializeField] TMP_Text _coinValueText;
 
         void OnValidate()
         {
@@ -14,13 +18,24 @@ namespace Platformer2D.Controllers
             if (_collider2D == null) _collider2D = GetComponent<Collider2D>();
         }
 
+        void Start()
+        {
+            _coinValueText.SetText(_spendCoin.ToString());
+        }
+
         void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.TryGetComponent(out PlayerController playerController)) return;
-            
+
+            int result = playerController.Coin - _spendCoin;
+
+            if (result < 0) return;
+
+            playerController.Coin = result;
+            _coinValueText.SetText(_spendCoin.ToString());
+
             _animator.SetTrigger("Open");
             _collider2D.enabled = false;
         }
-    }    
+    }
 }
-
