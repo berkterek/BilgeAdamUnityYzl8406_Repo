4f5e0a67991d3ff064f42de17a200d62b41af35d
berkterek <42406263+berkterek@users.noreply.github.com>
2023-07-30 13:43:09 +0300

    refactor: door can open and open sprites and open logic done

--- Platformer2D/Assets/_GameFolders/Scripts/Concretes/Controllers/DoorController.cs
+++ Platformer2D/Assets/_GameFolders/Scripts/Concretes/Controllers/DoorController.cs
@@ -3,25 +3,37 @@ using Platformer2D.Enums;
 using Platformer2D.ScriptableObjects;
 using UnityEngine;
 
-namespace Platformer2D.Controllers
+namespace Platformer2D.Controllers  
 {
     public class DoorController : MonoBehaviour
     {
-        [SerializeField] bool _canOpen;
+        [SerializeField] bool _canOpen = false;
+        [SerializeField] bool _canEnter = false;
+        [SerializeField] Collider2D _openCollider;
+        [SerializeField] Collider2D _enterCollider;
         [SerializeField] Sprite[] _doorSprites;
         [SerializeField] SpriteRenderer[] _doorSpriteRenderers;
         [SerializeField] List<ItemType> _itemTypes;
 
         void OnTriggerEnter2D(Collider2D other)
         {
-            if (!other.TryGetComponent(out PlayerController _playerController)) return;
+            if (!other.TryGetComponent(out PlayerController playerController)) return;
 
+            OpenDoorProcess(playerController);
+
+            if (!_canEnter) return;
+            
+            Debug.Log("Player can enter door and pass to next level");
+        }
+
+        private void OpenDoorProcess(PlayerController playerController)
+        {
             if (!_canOpen) return;
 
             List<ItemDataContainer> itemDataContainers = new List<ItemDataContainer>();
             foreach (var itemType in _itemTypes)
             {
-                var item = _playerController.Inventory.GetItemFromSlot(itemType);
+                var item = playerController.Inventory.GetItemFromSlot(itemType);
 
                 if (item != null)
                 {
@@ -40,6 +52,15 @@ namespace Platformer2D.Controllers
             if (_itemTypes.Count <= 0)
             {
                 Debug.Log("Start door open process");
+                for (int i = 0; i < _doorSpriteRenderers.Length; i++)
+                {
+                    _doorSpriteRenderers[i].sprite = _doorSprites[i];
+                }
+
+                _openCollider.enabled = false;
+                _enterCollider.enabled = true;
+                _canOpen = false;
+                _canEnter = true;
             }
         }
     }
