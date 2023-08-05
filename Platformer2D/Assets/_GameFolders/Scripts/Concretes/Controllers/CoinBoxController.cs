4c787e9d868ba848c5bab9d1ff4c92bfd210c5e6
berkterek <42406263+berkterek@users.noreply.github.com>
2023-08-05 12:32:09 +0300

    refactor: box coin closed or disable sprite added

--- Platformer2D/Assets/_GameFolders/Scripts/Concretes/Controllers/CoinBoxController.cs
+++ Platformer2D/Assets/_GameFolders/Scripts/Concretes/Controllers/CoinBoxController.cs
@@ -1,21 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Platformer2D.Controllers
 {
     public class CoinBoxController : BaseCoinController
     {
+        [SerializeField] Sprite _closedSprite;
+        [SerializeField] SpriteRenderer _spriteRenderer;
         [SerializeField] Animator _animator;
+        [SerializeField] bool _canCollected = true;
         
-        bool _canCollected;
-
         void OnValidate()
         {
             if (_animator == null) _animator = GetComponent<Animator>();
+            if (_spriteRenderer == null) _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         }
-
+        
         void OnCollisionEnter2D(Collision2D other)
         {
-            if (_canCollected) return;
+            if (!_canCollected) return;
             
             if (other.collider.TryGetComponent(out PlayerController playerController))
             {
@@ -29,8 +32,19 @@ namespace Platformer2D.Controllers
         protected override void AfterCoinState()
         {
             base.AfterCoinState();
-            _canCollected = _currentCoinCounter <= 0;
+            _canCollected = _currentCoinCounter > 0;
             _animator.SetTrigger("Collected");
+
+            if (!_canCollected)
+            {
+                StartCoroutine(WaitAndSetClosedSprite());
+            }
+        }
+
+        private IEnumerator WaitAndSetClosedSprite()
+        {
+            yield return new WaitForSeconds(1f);
+            _spriteRenderer.sprite = _closedSprite;
         }
     }
 }
